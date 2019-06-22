fb8f066d22d9810efc280e678d13fcae2acc0982
vasyanch <vasyanch852@gmail.com>
2019-06-22 15:22:36 +0300

    Add viewsets and GenericAPIView versions

--- test_django_GenericAPIView/article/urls.py
+++ test_django_GenericAPIView/article/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ArticleView
+from .views import ArticleView, SingleArticleView
 
 
 app_name = 'articles'
 
 urlpatterns = [
     path('articles/', ArticleView.as_view()),
-    path('articles/<int:pk>', ArticleView.as_view()),
+    path('articles/<int:pk>', SingleArticleView.as_view()),
 ]
--- test_django_GenericAPIView/article/views.py
+++ test_django_GenericAPIView/article/views.py
@@ -1,13 +1,18 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404
 
-from .models import Article
+from .models import Article, Author
 from .serializers import ArticleSerializer
 
 
-class ArticleView(ListModelMixin, GenericAPIView):
+class ArticleView(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
+
+
+class SingleArticleView(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
